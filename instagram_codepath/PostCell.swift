4c3126edf94eb1107748cf9db757f153c08c09d5
sgome121 <sgome121@fiu.edu>
2022-10-11 16:15:44 -0400

    Unit 5 code

---
+++ instagram_codepath/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  instagram_codepath
+//
+//  Created by sebastian on 10/11/22.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
